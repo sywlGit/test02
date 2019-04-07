b1fdbf93914b6b0e8e78ac1f9fc72ed09d52d808
sun <sun@itheima.com>
2019-04-07 10:49:10 +0800

    修改

--- src/main/java/com/itheima/test/Test.java
+++ src/main/java/com/itheima/test/Test.java
@@ -4,4 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("1111");
     }
+
+    public void test01(){
+        System.out.println("2222");
+    }
 }
